c322711ebec4bb0daaf382f0e3c34ccdf386fd11
Fintan O'Hea <fintanohea@gmail.com>
2020-02-05 23:56:18 -0800

    adding contact form, reorganising services

--- src/app/contact-details/contact-details.component.ts
+++ src/app/contact-details/contact-details.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-
-import { Hero }         from '../hero';
-import { HeroService }  from '../hero.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-details',
@@ -11,30 +6,10 @@ import { HeroService }  from '../hero.service';
   styleUrls: [ './contact-details.component.css' ]
 })
 export class ContactDetailsComponent implements OnInit {
-  @Input() hero: Hero;
-
   constructor(
-    private route: ActivatedRoute,
-    private heroService: HeroService,
-    private location: Location
   ) {}
 
   ngOnInit(): void {
-    this.getHero();
   }
 
-  getHero(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
-  }
-
-  goBack(): void {
-    this.location.back();
-  }
-
- save(): void {
-    this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
-  }
 }
--- src/app/services/hero-service/hero.service.ts
+++ src/app/services/hero-service/hero.service.ts
@@ -4,8 +4,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
-import { Hero } from './hero';
-import { MessageService } from './message.service';
+import { Hero } from '../../modals/hero';
+import { MessageService } from '../message-service/message.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
