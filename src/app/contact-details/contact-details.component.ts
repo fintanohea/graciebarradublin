f56996da2e439d126c316eec5ceb02f313d97d2c
Fintan O'Hea <fintanohea@gmail.com>
2020-02-13 16:48:23 -0800

    adding basic contact details section

--- src/app/contact-details/contact-details.component.ts
+++ src/app/contact-details/contact-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
-  styleUrls: [ './contact-details.component.css' ]
+  styleUrls: [ './contact-details.component.scss' ]
 })
 export class ContactDetailsComponent implements OnInit {
   constructor(
