9ef1530943f635ee528547119bc01940f38d7575
Fintan O'Hea <fintanohea@gmail.com>
2020-02-25 15:49:06 -0800

    changed about background, added dot sub-nav, added underline animation, added underline to about section

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,6 +1,4 @@
 import { Component, AfterViewInit, ViewChild, HostListener } from '@angular/core';
-import { SelectMultipleControlValueAccessor } from '@angular/forms';
-import { timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'app-homepage',
@@ -8,15 +6,38 @@ import { timeout } from 'rxjs/operators';
   styleUrls: [ './homepage.component.scss' ]
 })
 export class HomepageComponent implements AfterViewInit {
+  @ViewChild('home', {static: false}) home: HTMLElement;
+  @ViewChild('about', {static: false}) about: HTMLElement;
+  @ViewChild('instructor', {static: false}) instructor: HTMLElement;
+  @ViewChild('programs', {static: false}) programs: HTMLElement;
+  @ViewChild('pricing', {static: false}) pricing: HTMLElement;
   @ViewChild('map', {static: false}) map: HTMLElement;
+  selectedNav: HTMLElement;
+  navSections: Array<HTMLElement>;
 
   constructor() { }
 
   ngAfterViewInit() {
+    this.navSections = [
+      this.home,
+      this.about,
+      this.instructor,
+      this.programs,
+      this.pricing,
+      this.map
+    ]
+
+    this.selectedNav = this.home.nativeElement;
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+   this.redrawDotNav();
   }
 
   scroll(el: HTMLElement) {
-    const scrollTo = el.offsetTop - 91;
+    const scrollTo = el.offsetTop - 56;
+    this.selectedNav = el;    
 
     window.scrollTo({
       top: scrollTo,
@@ -24,4 +45,27 @@ export class HomepageComponent implements AfterViewInit {
       behavior: 'smooth'
     });
   }
+
+  redrawDotNav() {
+    const windowPosition = window.pageYOffset;
+    let section1Pos;
+    let section2Pos;
+
+    for ( let x = 0; x < this.navSections.length; x++ ) {
+      if ( x != this.navSections.length-1 ) {
+        section1Pos = this.navSections[x].nativeElement.offsetTop - 56;
+        section2Pos = this.navSections[x+1].nativeElement.offsetTop - 56;
+
+        if ( windowPosition >= section1Pos && windowPosition < section2Pos ) {
+          this.selectedNav = this.navSections[x].nativeElement;
+        }
+      } else {
+        if ( windowPosition >= section2Pos ) {
+          this.selectedNav = this.navSections[x].nativeElement;
+        }
+      } 
+    }
+
+  }
+
 }
